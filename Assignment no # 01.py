c970b194cd6f7024411098b0c34d0ce5253145c9
Zainab-Musleha <96945383+Zainab-Musleha@users.noreply.github.com>
2022-01-01 06:39:40 -0800

    Assignment no # 01

---
+++ Assignment no # 01.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+print("Hello World");
+
+
+# In[2]:
+
+
+print ('''Twinkle, twinkle, little star,
+                   How I wonder what you are!
+                        Up above the world so high,
+                        Like a diamond in the sky.
+Twinkle, twinkle, little star,
+         How I wonder what you are''');
+
+
+# In[3]:
+
+
+print ("Python Version = 3.7");
+
+
+# In[4]:
+
+
+print (" Date : 1-1-2022 , Time : 10:19 a.m");
+
+
+# In[8]:
+
+
+pi=3.14
+r=float(input("Enter the radius"))
+area=pi*r*r
+print("Area of a circle=%.2f"%area)
+
+
+# In[9]:
+
+
+x = " Zainab";
+y = "Musleha";
+z = y + x;
+print (z);
+
+
+# In[10]:
+
+
+x = 5;
+y = 6;
+z = x + y;
+print (z);
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
